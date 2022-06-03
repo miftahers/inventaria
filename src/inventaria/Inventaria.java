839c351c1e24a9eb80abcdfc189d921ccbedc9c9
sndff <sndff@upi.edu>
2022-06-03 20:07:43 +0700

    Ubah nama Package+class

--- src/inventaria/Inventaria.java
+++ src/inventaria/Inventaria.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inventaria1;
+package inventaria;
 
 /**
  *
  * @author saifer
  */
-public class Inventaria1 {
+public class Inventaria {
 
     /**
      * @param args the command line arguments
